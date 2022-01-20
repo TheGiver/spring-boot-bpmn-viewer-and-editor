f3ee9257504f0f63c26f5e3b3dc1b21c56542245
TheGiver <faysalkharraz@gmail.com>
2022-01-20 08:55:16 +0200

    defining the general structure of each component via the <component-name>.ts files.

---
+++ bpmn-viewer-and-editor/src/app/editor-component/editor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'bpmn-editor-component',
+    templateUrl: './editor.component.html',
+    styleUrls: ['./editor.component.css']
+})
+
+export class EditorComponent {
+    
+}
---
+++ bpmn-viewer-and-editor/src/app/viewer-component/viewer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'bpmn-viewer-component',
+    templateUrl: './viewer.component.html',
+    styleUrls: ['./viewer.component.css']
+})
+
+export class ViewerComponent {
+    
+}
